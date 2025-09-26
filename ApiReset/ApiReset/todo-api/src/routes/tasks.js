685eda3c25dfe7fd6e7197d8dd70e504a5a2e514
Gustavo Moura <159397965+GuhMoura@users.noreply.github.com>
2025-09-25 21:42:34 -0300

    update

--- ApiReset/ApiReset/todo-api/src/routes/tasks.js
+++ ApiReset/ApiReset/todo-api/src/routes/tasks.js
@@ -1,4 +1,4 @@
-// Importa as ferramentas que vamos precisar do Express e do Node.js
+
 import { Router } from 'express'; // O Router é como uma "central de rotas" do Express.
 import { randomUUID } from 'crypto';  
 //Usado para gerar IDs únicos para cada tarefa. -> Uma forma de não precisar usar o BD, RandomUIID gera um id aleatório que vai na bliblioteca crypto. 
@@ -6,9 +6,7 @@ import { randomUUID } from 'crypto';
 // central de rotas usado para organizarmos nossos endpoints de tarefas.
 const router = Router();
 
-// --- NOSSA BD sem BD  ---
-// Aqui é onde vamos guardar nossas tarefas.
-// Os array será nossa "memória".
+// --- NOSSA BD sem BD (Tive que batalhar para saber como faz)  ---
 // Toda vez que o servidor reiniciar, ele começará vazio novamente.
 let tarefas = [];
 
@@ -17,7 +15,7 @@ let tarefas = [];
 // ROTA 1: CRIAR uma nova tarefa  
 // Gustavo Não esquecer Post -> HTTP: POST
 router.post('/', (req, res) => {
-  // Pega o título e a descrição que o usuário enviou no corpo (body) da requisição.
+  
   const { title, description } = req.body;
 
   // Validação: Verifica se o título e a descrição foram enviados.
@@ -40,11 +38,11 @@ router.post('/', (req, res) => {
   // Adiciona a nova tarefa ao nosso array de "memória".
   tarefas.push(novaTarefa);
 
-  // Retorna a tarefa recém-criada com o status 201 (Criado com Sucesso).
+  // Retorna a tarefa criada com o status 201 (Criado com Sucesso).
   res.status(201).json(novaTarefa);
 });
 
-// ROTA 2: LISTAR todas as tarefas
+
 // Gustavo Não esquecer Post -> HTTP: GET
 router.get('/', (req, res) => {
   // retorna a lista completa de tarefas com o status 200 (OK).
@@ -52,7 +50,7 @@ router.get('/', (req, res) => {
 });
 
 
-// --- ROTAS AGRUPADAS & ID específico ---
+
 // Usamos router.route() para agrupar PUT e DELETE que usam o mesmo caminho: '/:id'
 router.route('/:id')
 
@@ -61,12 +59,12 @@ router.route('/:id')
   .put((req, res) => {
     // Pega o ID que veio na URL (ex: /tasks/Corinthians2012)
     const { id } = req.params;
-    // Pega os novos dados que o usuário enviou no corpo da requisição.
+    
     const { title, description, completed } = req.body;
 
     // Procura no array a posição (índice) da tarefa que tem o mesmo ID.
     const indiceTarefa = tarefas.findIndex(tarefa => tarefa.id === id);
-    //Ele percorre um array, item por item, e executa uma função de teste para cada um .
+  
 
     // Validação: Se o findIndex não encontrar a tarefa, ele retorna -1.
     if (indiceTarefa === -1) {
@@ -95,13 +93,12 @@ router.route('/:id')
     res.status(200).json(tarefaAtualizada);
   })
 
-  // ROTA 4: EXCLUIR uma tarefa pelo ID
+  
   // Gustavo Não esquecer Post -> HTTP: DELETE
   .delete((req, res) => {
-    // Pega o ID que veio na URL.
+    
     const { id } = req.params;
 
-    // Cria um novo array contendo todas as tarefas, EXCETO aquela que tem o ID que queremos excluir.
     const novasTarefas = tarefas.filter(tarefa => tarefa.id !== id);
     //Filter -> ele copia a lista original e se for false, a tarefa é deixada para trás, fazendo assim o delete.
 
